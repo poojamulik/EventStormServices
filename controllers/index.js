4c45b30ae19d80a452f350fb74be800f01c58d89
Pooja <pmulik2288@gmail.com>
2015-07-13 22:36:55 +0800

    second commit

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@
 
 var IndexModel = require('../models/index');
 var EventTypeModel = require('../models/eventType');
+var EventModel = require('../models/events');
 
 module.exports = function (router) {
 
     var model = new IndexModel();
     var eventTypeModel = new EventTypeModel();
+    var eventModel = new EventModel();
 
     router.get('/', function (req, res) {
         
@@ -23,6 +25,12 @@ module.exports = function (router) {
     router.get('/eventType', function (req, res) {
         
         res.send('<code><pre>' + JSON.stringify(eventTypeModel, null, 2) + '</pre></code>');
+        
+    });
+
+    router.get('/events', function (req, res) {
+        
+        res.send('<code><pre>' + JSON.stringify(eventModel, null, 2) + '</pre></code>');
         
     });
 

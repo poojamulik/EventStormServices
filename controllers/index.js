d505c5857eeda6eff931aae0242ad3c78c34ca85
Pooja <pmulik2288@gmail.com>
2015-07-13 23:02:59 +0800

    1

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,12 @@ var IndexModel = require('../models/index');
 var EventTypeModel = require('../models/eventType');
 var EventModel = require('../models/events');
 
+
+var fs = require('fs');
+var path = require('path');
+var eventFilePath = path.join("c:/Pooja/node/nno/models", 'event.json');
+var filePath = path.join("c:/Pooja/node/nno/models", 'eventType.json');
+
 module.exports = function (router) {
 
     var model = new IndexModel();
@@ -22,7 +28,7 @@ module.exports = function (router) {
         
     });
 
-    router.get('/eventType', function (req, res) {
+    /*router.get('/eventType', function (req, res) {
         
         res.send('<code><pre>' + JSON.stringify(eventTypeModel, null, 2) + '</pre></code>');
         
@@ -32,6 +38,28 @@ module.exports = function (router) {
         
         res.send('<code><pre>' + JSON.stringify(eventModel, null, 2) + '</pre></code>');
         
+    });*/
+
+    router.get('/events', function (req, res) {
+        fs.readFile(eventFilePath, {encoding: 'utf-8'}, function(err,data){
+           if (!err){
+               var str = data;
+               res.json(JSON.parse(str));
+            }else{
+                console.log(err);
+            };
+        });
+    });
+
+    router.get('/eventType', function (req, res) {
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+           if (!err){
+               var str = data;
+               res.json(JSON.parse(str));
+            }else{
+                console.log(err);
+            };
+        });
     });
 
 };
